2266d839f7b7c6811e203924c4bb2778e10f5960
david <dg071018@gmail.com>
2025-03-14 10:34:35 +0100

    Add content filter to append plugin message to posts
    Introduced a new function `myUniqueDavidPlugin` that appends a custom message to the content of posts using the `the_content` filter. This enhances the plugin's functionality by dynamically modifying post content.

--- wp-content/plugins/my-unique-david-pligin/my-unique-david-plugin.php
+++ wp-content/plugins/my-unique-david-pligin/my-unique-david-plugin.php
@@ -5,3 +5,8 @@ Description: This is my first plugin
 Author: Ivan David Guzman Ruiz
 Version: 1.0
  */
+add_filter('the_content', 'myUniqueDavidPlugin');
+function myUniqueDavidPlugin($content) {
+    $content .= 'This is my first plugin';
+    return $content;
+}
