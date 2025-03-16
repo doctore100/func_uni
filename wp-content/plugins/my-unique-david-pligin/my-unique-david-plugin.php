d40ca3b3eee3be3ea7af20149c4762fd8e95ddfa
david <dg071018@gmail.com>
2025-03-16 11:12:50 +0100

    Add settings and fields for 'wcp_location' and 'wcp_headline'
    Introduce new settings 'wcp_location' and 'wcp_headline' with associated fields, enabling configurable options for display location and headline text. This update improves plugin customization and user control over content settings.

--- wp-content/plugins/my-unique-david-pligin/my-unique-david-plugin.php
+++ wp-content/plugins/my-unique-david-pligin/my-unique-david-plugin.php
@@ -31,6 +31,9 @@ class wordCountUniquePlugin {
 	}
 
 	function settings(): void {
+		/**
+		 * Registers the 'wcp_location' setting and adds a settings field for it.
+		 */
 		register_setting(
 			'wordCountPlugin',
 			'wcp_location',
@@ -41,12 +44,51 @@ class wordCountUniquePlugin {
 			)
 		);
 
+		/**
+		 * Adds a settings field for the 'wcp_location' option.
+		 *
+		 * @param string $id The field ID.
+		 * @param string $title The title of the field.
+		 * @param callable $callback Function to generate the field's HTML.
+		 * @param string $page The menu page.
+		 * @param string $section The section on the menu page.
+		 */
 		add_settings_field(
 			'wcp_location',
 			'Display Location',
 			array( $this, 'locationHTML' ),
 			'my-unique-david-plugin',
 			'wcp_first_section'
+		);
+
+
+		/**
+		 * Registers the 'wcp_headline' setting and adds a settings field for it.
+		 */
+		register_setting(
+			'wordCountPlugin',  // The settings group name.
+			'wcp_headline',     // The option name in the database.
+			array(
+				'sanitize_callback' => 'sanitize_text_field', // Callback to sanitize the field input.
+				'default'           => '0'                   // Default value for the setting.
+			)
+		);
+
+		/**
+		 * Adds a settings field for the 'wcp_headline' option.
+		 *
+		 * @param string $id The field ID.
+		 * @param string $title The title of the field displayed in the UI.
+		 * @param callable $callback Function to generate the field's HTML.
+		 * @param string $page The menu page where the settings field is displayed.
+		 * @param string $section The section on the menu page where the field is displayed.
+		 */
+		add_settings_field(
+			'wcp_headline',            // Field ID.
+			'Headline Text',           // Title of the field.
+			array( $this, 'headlineHTML' ), // Callback function to render the field's HTML.
+			'my-unique-david-plugin',  // Page on which to display the field.
+			'wcp_first_section'        // Section to place the field in.
 		);
 
 		add_settings_section(
@@ -76,11 +118,19 @@ class wordCountUniquePlugin {
 	function locationHTML() {
 		?>
         <select name="wcp_location">
-            <option value="0">Beginning of Post</option>
-            <option value="1">End of Post</option>
+            <option value="0" <?php selected( get_option( 'wcp_location' ), '0' ) ?>>Beginning of Post</option>
+            <option value="1"<?php selected( get_option( 'wcp_location' ), '1' ) ?>>End of Post</option>
         </select>
 		<?php
 	}
+    function headlineHTML() {
+	    ?>
+        <select name="wcp_location">
+            <option value="0" <?php selected( get_option( 'wcp_location' ), '0' ) ?>>Beginning of Post</option>
+            <option value="1"<?php selected( get_option( 'wcp_location' ), '1' ) ?>>End of Post</option>
+        </select>
+	    <?php
+    }
 }
 
 $wordCountUniquePlugin = new wordCountUniquePlugin();
