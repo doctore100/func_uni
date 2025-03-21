d7390d4d104f0b969c35e9587ff3afa760186bf8
david <dg071018@gmail.com>
2025-03-21 18:33:06 +0100

    Add initial version of text filter plugin
    Introduce the first implementation of the Fictional University Text Filter plugin. This plugin utilizes JavaScript and PHP to create a dynamic filtering functionality. Includes metadata for author, version, and text domain.

---
+++ wp-content/plugins/our-word-filter-plugin/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Plugin Fictional University Text Filter
+ * Description: This is my second plugin, which is a plugin that uses JavaScript and PHP to create a dynamic filter.
+ * Author: Iván David Guzmán Ruiz
+ * Version: 1.0
+ * Text Domain: domaindavid
+ * Domain Path: /languages
+ */
