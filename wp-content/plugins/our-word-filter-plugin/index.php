16895c3b4370748a1fbcf8958350cba04d976b24
david <dg071018@gmail.com>
2025-03-22 12:30:14 +0100

    Add settings section and filtering logic to plugin
    Introduced a new settings section for managing filter options, allowing admin users to input and save forbidden words. Added logic to replace specified words in content with a placeholder and improved form handling with nonce verification for security.

--- wp-content/plugins/our-word-filter-plugin/index.php
+++ wp-content/plugins/our-word-filter-plugin/index.php
@@ -18,6 +18,7 @@ class Our_Word_Filter_Plugin {
 	// Size icon 20x20 px
 	const string SVG_ICON = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+';
 
+	const string SECTION_ID = 'replace-text-section';
 
 	/**
 	 * Constructor - initialize hooks
@@ -25,8 +26,55 @@ class Our_Word_Filter_Plugin {
 	public function __construct() {
 		// Register the admin menu
 		add_action( 'admin_menu', [ $this, 'register_admin_menu' ] );
-		// Clean cache
-//		add_action( 'update_option_wcp_location', [ $this, 'clean_cache_after_location_change' ], 10, 2 );
+		add_action( 'admin_init', [ $this, 'register_settings' ] );
+		if ( get_option( 'plugin_word_to_filter' ) ) {
+			add_filter( 'the_content', [ $this, 'filter_logic' ] );
+		}
+
+
+	}
+
+	/**
+	 * Register settings sections for the plugin options page.
+	 *
+	 * This method creates a settings section where plugin options
+	 * will be registered. The section has no title or callback
+	 * as these will be handled by individual settings fields.
+	 */
+	public function register_settings(): void {
+		add_settings_section(
+			self::SECTION_ID,       // ID of the settings section
+			null,             // No section title needed
+			null,             // No section callback needed
+			self::PLUGIN_SLUG_OPTIONS  // Page slug where to show the section
+		);
+		register_setting(
+			'replace-field',
+			'replace-text'
+		);
+		add_settings_field(
+			'replace-text-field',
+			'Word to filter',
+			[ $this, 'replace_field_html' ],
+			self::PLUGIN_SLUG_OPTIONS,
+			self::SECTION_ID
+		);
+	}
+
+	public function replace_field_html(): void {
+		$replace_text = get_option( 'replace-text', '***' );
+		?>
+        <label for="replace_text_id"></label>
+        <input id="replace_text_id" type="text" name="replace-text" value="<?php echo esc_attr( $replace_text ); ?>">
+        <p class="description">Leave blank to simply remove the filter word</p>
+		<?php
+	}
+
+	public function filter_logic( $content ): array|string {
+		$bad_words = explode( ',', get_option( 'plugin_word_to_filter' ) );
+		$bad_words_trim = array_map( 'trim', $bad_words );
+		return str_ireplace( $bad_words_trim, '***', $content );
+
 
 	}
 
@@ -52,7 +100,7 @@ class Our_Word_Filter_Plugin {
 			'Word to Filter',        // Page title
 			'Word List',                        // Menu title
 			'manage_options',                 // Capability required
-			self::PLUGIN_SLUG_OPTIONS,        // Menu slug
+			self::PLUGIN_SLUG,        // Menu slug
 			[ $this, 'render_main_page' ]    // Callback function
 		);
 
@@ -72,27 +120,44 @@ class Our_Word_Filter_Plugin {
 		wp_enqueue_style( 'filterAdminCss', plugin_dir_url( __FILE__ ) . "css/admin.css" );
 	}
 
+	public function handle_submit() {
+		if ( wp_verify_nonce( $_POST['plugin_word_filter_nonce'], 'plugin_word_filter_action' ) && current_user_can( 'manage_options' ) ) {
+			update_option( 'plugin_word_to_filter', sanitize_text_field( trim( $_POST['plugin_word_to_filter'] ) ) ); ?>
+            <div class="updated">
+                <p>The word has been saved</p>
+            </div>
+		<?php } else { ?>
+            <div class="error">
+                <p>Sorry you don't have permission for that action</p>
+            </div>
+
+		<?php }
+
+	}
+
 	/**
 	 * Render the main plugin page
 	 */
 	public function render_main_page(): void {
 		?>
-        <section>
+        <section class="wrap">
             <h1> Word Filter</h1>
 			<?php
 			$is_form_submitted = isset( $_POST['form_submitted'] ) && $_POST['form_submitted'] === 'true';
 			if ( $is_form_submitted ) {
-				echo "<p class='notice notice-success'>Thank you for your submission!</p>";
-			}
-			?>
+				$this->handle_submit();
+			} ?>
             <p>This is the main page of the plugin</p>
             <form method="post">
                 <input type="hidden" name="form_submitted" value="true">
-                <label for="plugin_word_to_filter">Enter <strong>separate by coma</strong> the word you want to
-                    fiend</label>
+
+				<?php wp_nonce_field( 'plugin_word_filter_action', 'plugin_word_filter_nonce' ); ?>
+                <label for="plugin_word_to_filter">
+                    Enter <strong>separate by coma</strong> the word you want to fiend
+                </label>
                 <div class="word-filter_flex-container">
                     <textarea name="plugin_word_to_filter" id="plugin_word_to_filter"
-                              placeholder="main, bock, teacher"></textarea>
+                              placeholder="main, bock, teacher"><?php echo esc_textarea( trim( get_option( "plugin_word_to_filter" ) ) ); ?></textarea>
                 </div>
                 <input type="submit" value="Save Changes" id="submit" class="button button-primary">
             </form>
@@ -106,9 +171,20 @@ class Our_Word_Filter_Plugin {
 	public function render_options_page(): void {
 		?>
         <div class="wrap">
-            <h1>Word Filter Options</h1>
-            <p>Configure your word filter options here.</p>
+
             <!-- Options form would go here -->
+            <div class="warp">
+                <h1>Word Filter Options</h1>
+                <p>Configure your word filter options here.</p>
+                <form action="options.pho" method="post">
+					<?php
+
+					submit_button(); ?>
+                    <?php settings_fields( 'replace-field' ); ?>
+                    <?php do_settings_sections( self::PLUGIN_SLUG_OPTIONS ); ?>
+                </form>
+
+            </div>
         </div>
 		<?php
 	}
