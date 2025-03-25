34d84380aa3931c3c456bdd84b0acd35ae3b3f4f
david <dg071018@gmail.com>
2025-03-25 23:19:07 +0100

    Add Block_React_Example class with admin asset enqueueing
    Introduces a class to encapsulate plugin functionality. Adds a method to enqueue block editor assets, ensuring the required script is properly loaded for the block. This improves code organization and prepares the plugin for future expansion.

--- wp-content/plugins/block-react-example/index.php
+++ wp-content/plugins/block-react-example/index.php
@@ -10,3 +10,12 @@
 if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
+class Block_React_Example {
+	function __construct() {
+		add_action('enqueue_block_editor_assets', array($this, 'admin_assets'));
+	}
+	public function admin_assets(): void {
+		wp_enqueue_script( 'block-react-example-block', plugin_dir_url(__FILE__ ). 'js/index.js', ['wp-blocks', 'wp-element'],  );
+	}
+}
+$block_react_example = new Block_React_Example();
