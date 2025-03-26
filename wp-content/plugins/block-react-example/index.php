75d938858c7ffe20dd0602aa1044285000b783c2
david <dg071018@gmail.com>
2025-03-26 09:36:50 +0100

    Refactor block registration and add render callback
    Moved block script registration from `enqueue_block_editor_assets` to `init` for proper initialization. Introduced `register_block_type` with a `render_callback` to dynamically render block content based on attributes.

--- wp-content/plugins/block-react-example/index.php
+++ wp-content/plugins/block-react-example/index.php
@@ -13,10 +13,17 @@ if ( !defined( 'ABSPATH' ) ) {
 
 class Block_React_Example {
 	function __construct() {
-		add_action('enqueue_block_editor_assets', array($this, 'admin_assets'));
+		add_action('init', array($this, 'admin_assets'));
 	}
 	public function admin_assets(): void {
-		wp_enqueue_script( 'block-react-example-block', plugin_dir_url(__FILE__ ). 'build/index.js', ['wp-blocks', 'wp-element'],  );
+		wp_register_script( 'block-react-example-block', plugin_dir_url(__FILE__ ). 'build/index.js', ['wp-blocks', 'wp-element'],  );
+		register_block_type('block-example/block-react-example', [
+			'editor_script' => 'block-react-example-block',
+			'render_callback' => [$this, 'render_block']
+		]);
+	}
+	public function render_block($attributes): string{
+		return '<h1>The sky is ' . $attributes['skyColor'] . 'and the grass is ' . $attributes['grassColor']. 'color </h1>';
 	}
 }
 $block_react_example = new Block_React_Example();
