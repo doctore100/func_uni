8bce8dacbb894f80cc9c489c8b24cba7fdcf4568
david <dg071018@gmail.com>
2025-03-26 08:16:48 +0100

    Update script path to use the build directory
    Updated the script enqueued in the plugin to reference the compiled JS file in the `build` directory instead of `js`. This ensures the correct file is loaded during block editor initialization.

--- wp-content/plugins/block-react-example/index.php
+++ wp-content/plugins/block-react-example/index.php
@@ -10,12 +10,13 @@
 if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
+
 class Block_React_Example {
 	function __construct() {
 		add_action('enqueue_block_editor_assets', array($this, 'admin_assets'));
 	}
 	public function admin_assets(): void {
-		wp_enqueue_script( 'block-react-example-block', plugin_dir_url(__FILE__ ). 'js/index.js', ['wp-blocks', 'wp-element'],  );
+		wp_enqueue_script( 'block-react-example-block', plugin_dir_url(__FILE__ ). 'build/index.js', ['wp-blocks', 'wp-element'],  );
 	}
 }
 $block_react_example = new Block_React_Example();
