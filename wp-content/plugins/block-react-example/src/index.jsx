d0bad15c74a78a8c4a57bd478e4346837dc46b0a
david <dg071018@gmail.com>
2025-03-26 09:36:28 +0100

    Refactor block attributes and remove save output.
    Introduced a cleaner attributes structure by defining them outside the block configuration. Removed the save function output to adjust block behavior, likely for dynamic or server-side rendering.

--- wp-content/plugins/block-react-example/src/index.jsx
+++ wp-content/plugins/block-react-example/src/index.jsx
@@ -1,11 +1,16 @@
+const attributes = {
+    skyColor: {
+        type: 'string',
+    },
+    grassColor: {
+        type: 'string',
+    }
+};
 wp.blocks.registerBlockType('block-example/block-react-example', {
     title: 'Block React Example',
     icon: 'smiley',
     category: 'common',
-    attributes: {
-        skyColor: {type: 'string', source: 'text', selector: ".skyColor" },
-        grassColor: {type: 'string', source: 'text', selector: ".grassColor"}
-    },
+    attributes: attributes,
     edit: function (props) {
         function updateSkyColor(e) {
             props.setAttributes({skyColor: e.target.value})
@@ -17,17 +22,15 @@ wp.blocks.registerBlockType('block-example/block-react-example', {
 
         return (
             <div>
-                <input type="text" placeholder="Color of the sky" value={props.attributes.skyColor} onChange={updateSkyColor}></input>
-                <input type="text" placeholder="Color of the grass" value={props.attributes.grassColor} onChange={updateGrassColor}></input>
+                <input type="text" placeholder="Color of the sky" value={props.attributes.skyColor}
+                       onChange={updateSkyColor}></input>
+                <input type="text" placeholder="Color of the grass" value={props.attributes.grassColor}
+                       onChange={updateGrassColor}></input>
             </div>
         )
     },
-    save: function (props) {
-        return (
-            <p>
-                The color of the sky is <span className="skyColor">{props.attributes.skyColor}</span> and the color of the grass
-                is <span className="grassColor">{props.attributes.grassColor}</span>
-            </p>
-        )
+    save: function () {
+        return null
     },
+
 })
