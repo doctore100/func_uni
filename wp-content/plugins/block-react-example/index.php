fcac65b7a4b4c393290c0d527d746884b86550c5
david <dg071018@gmail.com>
2025-04-27 12:47:48 +0200

    Simplify block registration and enqueue logic.
    Removed redundant script and style registrations for better efficiency. Updated `register_block_type` to use `__DIR__` and cleaned up unnecessary enqueues in the render method.

--- wp-content/plugins/block-react-example/index.php
+++ wp-content/plugins/block-react-example/index.php
@@ -14,45 +14,25 @@ if ( !defined( 'ABSPATH' ) ) {
 class Block_React_Example {
 	function __construct() {
 		add_action(
-                'init',
-                [ $this, 'admin_assets' ]
-        );
+			'init',
+			[ $this, 'admin_assets' ]
+		);
 	}
 
 	public function admin_assets(): void {
-		wp_register_style(
-			'edit-css',
-			plugin_dir_url( __FILE__ ) . 'build/index.css'
-		);
 
-		wp_register_script(
-			'block-react-example-block',
-			plugin_dir_url( __FILE__ ) . 'build/index.js',
-			[ 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components' ],
-			'1.0.0',
-			true
-		);
 
 		register_block_type(
-			'block-example/block-react-example',
+			__DIR__,
 			[
-				'editor_style' => 'edit-css',
-				'editor_script' => 'block-react-example-block',
+
 				'render_callback' => [ $this, 'render_block' ]
 			]
 		);
 	}
 
 	public function render_block( $attributes ): string {
-		wp_enqueue_script(
-			'FrontEndScript',
-			plugin_dir_url( __FILE__ ) . 'build/FrontEnd.js',
-			[ 'wp-element', 'react-jsx-runtime', 'wp-components' ],
-		);
-		wp_enqueue_style(
-			'FrontEndStyles',
-			plugin_dir_url( __FILE__ ) . 'build/FrontEnd.css',
-		);
+
 
 		ob_start(); ?>
         <div class="paying-attention-me">
